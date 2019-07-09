271d5244c90350c014676517adb23024d88acfa5
Alex Vlasov <alex.m.vlasov@gmail.com>
2019-07-09 22:05:17 +0300

    Initial Commit

---
+++ eth_pairings/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  eth_pairings
+//
+//  Created by Alexander on 09/07/2019.
+//  Copyright © 2019 Alexander. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
